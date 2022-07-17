10456072c08dd8319e68e6695f62e2c9bccdd45c
slowy07 <slowy.arfy@gmail.com>
2022-07-18 03:05:22 +0700

    feat: create game
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

---
+++ support.py
@@ -0,0 +1,14 @@
+from os import walk
+import pygame
+
+
+def import_folder(path):
+    surface_list = []
+
+    for _, _, img_files in walk(path):
+        for image in img_files:
+            full_path = path + "/" + image
+            image_surf = pygame.image.load(full_path).convert_alpha()
+            surface_list.append(image_surf)
+
+    return surface_list
